ff865b056430879a7cc18aec36dab62bcae506dc
jlaskow <136547278+jlaskow@users.noreply.github.com>
2024-12-24 20:29:31 -1000

    Create scripts.js

---
+++ projects/scripts.js
@@ -0,0 +1,9 @@
+// jQuery Library
+let scriptJquery = document.createElement('script');
+scriptJquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js';
+document.head.appendChild(scriptJquery);
+
+// Bootstrap JS Library
+let scriptBootstrap = document.createElement('script');
+scriptBootstrap.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js';
+document.head.appendChild(scriptBootstrap);
